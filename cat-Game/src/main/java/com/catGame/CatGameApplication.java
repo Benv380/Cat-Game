a2a1e54cae3ecf82282d8b3572eb04b71d0929ab
Benjamin Marfull <167047801+Benv380@users.noreply.github.com>
2025-05-30 23:03:38 -0400

    Add files via upload

---
+++ cat-Game/src/main/java/com/catGame/CatGameApplication.java
@@ -0,0 +1,13 @@
+package com.catGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatGameApplication.class, args);
+	}
+
+}
---
+++ cat-Game/src/test/java/com/catGame/CatGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
